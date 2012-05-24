f06c0a7ea4eee015abd8120f29a6fb3c18c4580d
Bernard Potocki <bernard.potocki@imanel.org>
2012-05-24 20:34:25 +0200

    Move more functionality to ring

--- src/org/imanel/towerofhanoi/Ring.java
+++ src/org/imanel/towerofhanoi/Ring.java
@@ -3,17 +3,21 @@ package org.imanel.towerofhanoi;
 import java.util.Stack;
 
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.region.ITextureRegion;
-import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
 
 public class Ring extends Sprite {
     private int weight;
     private Stack stack;
     private Sprite tower;
+    private TowerOfHanoiActivity parent;
 
-    public Ring(int w, float pX, float pY, ITextureRegion pTextureRegion, VertexBufferObjectManager pVertexBufferObjectManager) {
-        super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
+    public Ring(int w, Sprite t, Stack s, ITextureRegion pTextureRegion, TowerOfHanoiActivity p) {
+        super(0, 0, pTextureRegion, p.getVertexBufferObjectManager());
+        parent = p;
         weight = w;
+        addTo(t, s);
     }
 
     public int getWeight() {
@@ -63,5 +67,36 @@ public class Ring extends Sprite {
     	s.add(this);
     	setStack(s);
     	setTower(t);
+    }
+    
+    @Override
+    public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+        if (((Ring) this.getStack().peek()).getWeight() != this.getWeight())
+            return false;
+        this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2,
+            pSceneTouchEvent.getY() - this.getHeight() / 2);
+        if (pSceneTouchEvent.getAction() == TouchEvent.ACTION_UP) {
+            checkForCollisionsWithTowers();
+        }
+        return true;
+    }
+    
+    private void checkForCollisionsWithTowers() {
+        Stack s = null;
+        Sprite t = null;
+        if (collidesWith(parent.tower1) && canBeAddedToStack(parent.stack1)) {
+            s = parent.stack1;
+            t = parent.tower1;
+        } else if (collidesWith(parent.tower2) && canBeAddedToStack(parent.stack2)) {
+            s = parent.stack2;
+            t = parent.tower2;
+        } else if (collidesWith(parent.tower3) && canBeAddedToStack(parent.stack3)) {
+            s = parent.stack3;
+            t = parent.tower3;
+        } else {
+            s = getStack();
+            t = getTower();
+        }
+        addTo(t, s);
     }
 }
--- src/org/imanel/towerofhanoi/TowerOfHanoiActivity.java
+++ src/org/imanel/towerofhanoi/TowerOfHanoiActivity.java
@@ -23,8 +23,8 @@ public class TowerOfHanoiActivity extends SimpleBaseGameActivity {
     private static int CAMERA_WIDTH = 800;
     private static int CAMERA_HEIGHT = 480;
     private ITextureRegion backgroundTextureRegion, towerTextureRegion, ring1TextureRegion, ring2TextureRegion, ring3TextureRegion;
-    private Sprite tower1, tower2, tower3;
-    private Stack stack1, stack2, stack3;
+    public Sprite tower1, tower2, tower3;
+    public Stack stack1, stack2, stack3;
 
     @Override
     public EngineOptions onCreateEngineOptions() {
@@ -58,6 +58,10 @@ public class TowerOfHanoiActivity extends SimpleBaseGameActivity {
         Sprite backgroundSprite = new Sprite(0, 0, backgroundTextureRegion, getVertexBufferObjectManager());
         scene.attachChild(backgroundSprite);
 
+        stack1 = new Stack();
+        stack2 = new Stack();
+        stack3 = new Stack();
+        
         tower1 = new Sprite(192, 63, towerTextureRegion, getVertexBufferObjectManager());
         tower2 = new Sprite(400, 63, towerTextureRegion, getVertexBufferObjectManager());
         tower3 = new Sprite(604, 63, towerTextureRegion, getVertexBufferObjectManager());
@@ -65,62 +69,12 @@ public class TowerOfHanoiActivity extends SimpleBaseGameActivity {
         scene.attachChild(tower2);
         scene.attachChild(tower3);
 
-        Ring ring1 = new Ring(1, 139, 174, ring1TextureRegion, getVertexBufferObjectManager()) {
-            @Override
-            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
-                if (((Ring) this.getStack().peek()).getWeight() != this.getWeight())
-                    return false;
-                this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2,
-                    pSceneTouchEvent.getY() - this.getHeight() / 2);
-                if (pSceneTouchEvent.getAction() == TouchEvent.ACTION_UP) {
-                    checkForCollisionsWithTowers(this);
-                }
-                return true;
-            }
-        };
-        Ring ring2 = new Ring(2, 118, 212, ring2TextureRegion, getVertexBufferObjectManager()) {
-            @Override
-            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
-                if (((Ring) this.getStack().peek()).getWeight() != this.getWeight())
-                    return false;
-                this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2,
-                    pSceneTouchEvent.getY() - this.getHeight() / 2);
-                if (pSceneTouchEvent.getAction() == TouchEvent.ACTION_UP) {
-                    checkForCollisionsWithTowers(this);
-                }
-                return true;
-            }
-        };
-        Ring ring3 = new Ring(3, 97, 255, ring3TextureRegion, getVertexBufferObjectManager()) {
-            @Override
-            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
-                if (((Ring) this.getStack().peek()).getWeight() != this.getWeight())
-                    return false;
-                this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2,
-                    pSceneTouchEvent.getY() - this.getHeight() / 2);
-                if (pSceneTouchEvent.getAction() == TouchEvent.ACTION_UP) {
-                    checkForCollisionsWithTowers(this);
-                }
-                return true;
-            }
-        };
+        Ring ring1 = new Ring(1, tower1, stack1, ring1TextureRegion, this);
+        Ring ring2 = new Ring(2, tower1, stack1, ring2TextureRegion, this);
+        Ring ring3 = new Ring(3, tower1, stack1, ring3TextureRegion, this);
         scene.attachChild(ring1);
         scene.attachChild(ring2);
         scene.attachChild(ring3);
-
-        stack1 = new Stack();
-        stack2 = new Stack();
-        stack3 = new Stack();
-        stack1.add(ring3);
-        stack1.add(ring2);
-        stack1.add(ring1);
-
-        ring1.setStack(stack1);
-        ring2.setStack(stack1);
-        ring3.setStack(stack1);
-        ring1.setTower(tower1);
-        ring2.setTower(tower1);
-        ring3.setTower(tower1);
 
         scene.registerTouchArea(ring1);
         scene.registerTouchArea(ring2);
@@ -128,25 +82,6 @@ public class TowerOfHanoiActivity extends SimpleBaseGameActivity {
         scene.setTouchAreaBindingOnActionDownEnabled(true);
 
         return scene;
-    }
-
-    private void checkForCollisionsWithTowers(Ring ring) {
-        Stack stack = null;
-        Sprite tower = null;
-        if (ring.collidesWith(tower1) && ring.canBeAddedToStack(stack1)) {
-            stack = stack1;
-            tower = tower1;
-        } else if (ring.collidesWith(tower2) && ring.canBeAddedToStack(stack2)) {
-            stack = stack2;
-            tower = tower2;
-        } else if (ring.collidesWith(tower3) && ring.canBeAddedToStack(stack3)) {
-            stack = stack3;
-            tower = tower3;
-        } else {
-            stack = ring.getStack();
-            tower = ring.getTower();
-        }
-        ring.addTo(tower, stack);
     }
     
     private ITexture preloadTexture(final String url) throws IOException {
