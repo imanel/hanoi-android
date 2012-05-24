f178eb67f156895306413ee4714e986b2ea4681d
Bernard Potocki <bernard.potocki@imanel.org>
2012-05-24 18:28:26 +0200

    Basic scene

--- src/org/imanel/towerofhanoi/TowerOfHanoiActivity.java
+++ src/org/imanel/towerofhanoi/TowerOfHanoiActivity.java
@@ -1,13 +1,98 @@
 package org.imanel.towerofhanoi;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.io.IOException;
+import java.io.InputStream;
 
-public class TowerOfHanoiActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.ITexture;
+import org.andengine.opengl.texture.bitmap.BitmapTexture;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.TextureRegionFactory;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.adt.io.in.IInputStreamOpener;
+import org.andengine.util.debug.Debug;
+
+public class TowerOfHanoiActivity extends SimpleBaseGameActivity {
+	private static int CAMERA_WIDTH = 800;
+	private static int CAMERA_HEIGHT = 480;
+	private ITextureRegion mBackgroundTextureRegion, mTowerTextureRegion, mRing1, mRing2, mRing3;
+	private Sprite mTower1, mTower2, mTower3;
+	
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
+	}
+	
+	@Override
+	protected void onCreateResources() {
+		try {
+		    ITexture backgroundTexture = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
+		        @Override
+		        public InputStream open() throws IOException {
+		            return getAssets().open("gfx/background.png");
+		        }
+		    });
+		    ITexture towerTexture = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
+		        @Override
+		        public InputStream open() throws IOException {
+		            return getAssets().open("gfx/tower.png");
+		        }
+		    });
+		    ITexture ring1 = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
+		        @Override
+		        public InputStream open() throws IOException {
+		            return getAssets().open("gfx/ring1.png");
+		        }
+		    });
+		    ITexture ring2 = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
+		        @Override
+		        public InputStream open() throws IOException {
+		            return getAssets().open("gfx/ring2.png");
+		        }
+		    });
+		    ITexture ring3 = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
+		        @Override
+		        public InputStream open() throws IOException {
+		            return getAssets().open("gfx/ring3.png");
+		        }
+		    });
+		    
+		    backgroundTexture.load();
+		    towerTexture.load();
+		    ring1.load();
+		    ring2.load();
+		    ring3.load();
+		    
+		    this.mBackgroundTextureRegion = TextureRegionFactory.extractFromTexture(backgroundTexture);
+			this.mTowerTextureRegion = TextureRegionFactory.extractFromTexture(towerTexture);
+			this.mRing1 = TextureRegionFactory.extractFromTexture(ring1);
+			this.mRing2 = TextureRegionFactory.extractFromTexture(ring2);
+			this.mRing3 = TextureRegionFactory.extractFromTexture(ring3);
+		} catch (IOException e) {
+		    Debug.e(e);
+		}
+	}
+	
+	@Override
+	protected Scene onCreateScene() {
+		final Scene scene = new Scene();
+		
+		Sprite backgroundSprite = new Sprite(0, 0, this.mBackgroundTextureRegion, getVertexBufferObjectManager());
+		scene.attachChild(backgroundSprite);
+		
+		mTower1 = new Sprite(192, 63, this.mTowerTextureRegion, getVertexBufferObjectManager());
+		mTower2 = new Sprite(400, 63, this.mTowerTextureRegion, getVertexBufferObjectManager());
+		mTower3 = new Sprite(604, 63, this.mTowerTextureRegion, getVertexBufferObjectManager());
+		scene.attachChild(mTower1);
+		scene.attachChild(mTower2);
+		scene.attachChild(mTower3);
+		
+		return scene;
+	}
 }
